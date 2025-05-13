84c2ece21c336c162817f062cb68d7d113603292
yaramohamadi <yara.mohamadi@gmail.com>
2025-05-13 11:19:34 -0400

    clean ablations

--- scripts/run_all_ablations.sh
+++ scripts/run_all_ablations.sh
@@ -2,32 +2,33 @@
 
 # ========== GLOBAL CONFIGURATION ==========
 SERVER="bool"
-CUDA_DEVICES="2,3"
+CUDA_DEVICES="1,2"
 SCRIPT="run_ours.sh"
-EXPERIMENT_PRENAME="ablation_mghigh"
 
-#     "stanford-cars_processed"
+
+#   "stanford-cars_processed" "cub-200-2011_processed"
 declare -a TASKS=(
-  "cub-200-2011_processed"
+  "stanford-cars_processed"
 )
 
-# ========== Define per-task (latestart, mghigh) pairs ==========
+# ========== Define per-task (latestart, mghigh, experiment_prename) triples ==========
 declare -A PAIR_MAP
-# Format: "latestart,mghigh latestart,mghigh ..."
-# PAIR_MAP["stanford-cars_processed"]="0,0.5 0,0.4 6000,1 7000 1"
-PAIR_MAP["cub-200-2011_processed"]="5000,0.75 6000,0.75, 7000,0.75 10000,0.75"
+# Format: "latestart,mghigh,prename latestart,mghigh,prename ..."
+# PAIR_MAP["cub-200-2011_processed"]="5000,0.75,ablation_latestart 6000,0.75,ablation_latestart 7000,0.75,ablation_latestart 10000,0.75,ablation_latestart"
+# PAIR_MAP["stanford-cars_processed"]="0,0.5,ablation_mghigh 0,0.4,ablation_mghigh 6000,1.0,ablation_latestart 7000,1.0,ablation_latestart 10000,1.0,ablation_latestart"
+PAIR_MAP["caltech-101_processed"]="0,0.4,ablation_mghigh 0,0.5,ablation_mghigh 0,0.6,ablation_mghigh 0,0.7,ablation_mghigh 0,0.8,ablation_mghigh 0,0.9,ablation_mghigh 0,1.0,ablation_mghigh"
+PAIR_MAP["artbench-10_processed"]="0,0.4,ablation_mghigh 0,0.5,ablation_mghigh 0,0.6,ablation_mghigh 0,0.7,ablation_mghigh 0,0.8,ablation_mghigh 0,0.9,ablation_mghigh 0,1.0,ablation_mghigh"
+PAIR_MAP["food-101_processed"]="0,0.4,ablation_mghigh 0,0.5,ablation_mghigh 0,0.6,ablation_mghigh 0,0.7,ablation_mghigh 0,0.8,ablation_mghigh 0,0.9,ablation_mghigh 0,1.0,ablation_mghigh"
+
 
 # ========== EXECUTION LOOP ==========
 for DATASET in "${TASKS[@]}"; do
   PAIRS=(${PAIR_MAP["$DATASET"]})
-  echo "$PAIRS"
   for PAIR in "${PAIRS[@]}"; do
-    LATESTART="${PAIR%%,*}"  # extract before comma
-    MGHIGH="${PAIR##*,}"     # extract after comma
-    
+    IFS=',' read -r LATESTART MGHIGH EXPERIMENT_PRENAME <<< "$PAIR"
 
     echo "=============================================="
-    echo "Running $SCRIPT on $DATASET | latestart: $LATESTART | mghigh: $MGHIGH"
+    echo "Running $SCRIPT on $DATASET | latestart: $LATESTART | mghigh: $MGHIGH | prename: $EXPERIMENT_PRENAME"
     echo "Server: $SERVER | CUDA Devices: $CUDA_DEVICES"
     echo "----------------------------------------------"
 
@@ -45,7 +46,7 @@ for DATASET in "${TASKS[@]}"; do
       eval "bash $CMD"
     fi
 
-    echo "✅ Finished $SCRIPT on $DATASET | latestart: $LATESTART | mghigh: $MGHIGH"
+    echo "✅ Finished $SCRIPT on $DATASET | latestart: $LATESTART | mghigh: $MGHIGH | prename: $EXPERIMENT_PRENAME"
     echo "=============================================="
   done
 done
