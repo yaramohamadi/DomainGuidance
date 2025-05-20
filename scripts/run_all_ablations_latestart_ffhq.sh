98293b567060ca5720efe61527f70e451b4394d9
yaramohamadi <yara.mohamadi@gmail.com>
2025-05-20 19:39:18 -0400

    FFHQ latestart

--- scripts/run_all_ablations_latestart_ffhq.sh
+++ scripts/run_all_ablations_latestart_ffhq.sh
@@ -5,15 +5,14 @@ SERVER="bool"
 CUDA_DEVICES="2,3"
 SCRIPT="run_ours.sh"
 
-
+ 
 declare -a TASKS=(
   "ffhq256"
 )
 
 # ========== Define per-task (latestart, mghigh, experiment_prename) triples ==========
 declare -A PAIR_MAP
-
-PAIR_MAP["ffhq256"]="0,0.4,ablation_mghigh 0,0.5,ablation_mghigh 0,0.6,ablation_mghigh 0,0.7,ablation_mghigh 0,0.8,ablation_mghigh 0,0.9,ablation_mghigh 0,1.0,ablation_mghigh"
+PAIR_MAP["ffhq256"]="1000,1,ablation_latestart 2000,1,ablation_latestart 3000,1,ablation_latestart 4000,1,ablation_latestart 5000,1,ablation_latestart 6000,1,ablation_latestart 7000,1,ablation_latestart"
 
 # ========== EXECUTION LOOP ==========
 for DATASET in "${TASKS[@]}"; do
@@ -34,11 +33,11 @@ for DATASET in "${TASKS[@]}"; do
       --latestart \"$LATESTART\" \
       --mghigh \"$MGHIGH\""
 
-    if [[ "$SERVER" == "computecanada" ]]; then
-      eval "JOB_NAME=$EXPERIMENT_PRENAME sbatch $CMD"
-    else
-      eval "bash $CMD"
-    fi
+    #if [[ "$SERVER" == "computecanada" ]]; then
+    #  eval "JOB_NAME=$EXPERIMENT_PRENAME sbatch $CMD"
+    #else
+    eval "bash $CMD"
+    #fi
 
     echo "✅ Finished $SCRIPT on $DATASET | latestart: $LATESTART | mghigh: $MGHIGH | prename: $EXPERIMENT_PRENAME"
     echo "=============================================="
