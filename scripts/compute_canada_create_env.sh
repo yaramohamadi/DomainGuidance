867d2c07d98b450674d70ac95cfccee3de76b88d
yaramohamadi <yara.mohamadi@gmail.com>
2025-05-28 13:38:17 -0400

    compute canada new envs

--- scripts/compute_canada_create_env.sh
+++ scripts/compute_canada_create_env.sh
@@ -15,22 +15,14 @@ module load StdEnv/2023 python/3.11.5
 python -m venv $ENV_PATH
 source $ENV_PATH/bin/activate
 
-pip install --no-index torch==2.6.0 timm==1.0.15 diffusers==0.32.2 accelerate==1.6.0 pytorch-fid==0.3.0  scikit-image==0.25.1 scikit-learn==1.6.1 transformers==4.52.3 xformers==0.0.29.post2 scipy==1.15.1 open_clip_torch==2.29.0 pandas==2.2.3 pillow==11.1.0
-
-# # Activate and install packages
-source "$ENV_PATH/bin/activate"
-
-pip install --upgrade pip --no-index
-pip install -r requirements.txt
-
-echo "✅ Environment '$ENV_PATH' set up from requirements.txt!"
+pip install --no-index --ignore-installed torch==2.6.0 timm==1.0.15 diffusers==0.32.2 accelerate==1.6.0 pytorch-fid==0.3.0  scikit-image==0.25.1 scikit-learn==1.6.1 transformers==4.52.3 xformers==0.0.29.post2 scipy==1.15.1 open_clip_torch==2.29.0 pandas==2.2.3 pillow==11.1.0
 
 # Install dgm-eval
 if [ ! -d "dgm-eval" ]; then
     git clone https://github.com/layer6ai-labs/dgm-eval.git
 fi
+
+cp scripts/compute_canada_create_env.py dgm-eval/setup.py
 pushd dgm-eval
-with open('setup.py', 'w') as f:
-    f.write(new_content)
 pip install --no-index -e .
 popd
