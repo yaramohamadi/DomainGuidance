70a7da8b994470179dd9ba8d95615f8915dd4330
yaramohamadi <yara.mohamadi@gmail.com>
2025-08-04 09:49:41 -0400

    clean up

--- scripts/Baselines_DiT_SiT.sh
+++ scripts/Baselines_DiT_SiT.sh
@@ -9,8 +9,8 @@ MODEL_NAME="DiT-XL/2" # or "SiT-XL/2"
 # ========== DATASET TO SCRIPT MAPPING ==========
 declare -a TASKS=(
   "food-101_processed run_baseline_MG.sh"
-  "food-101_processed run_baselines_finetune.sh"
 )
+#   "food-101_processed run_baselines_finetune.sh"
 
 # ========== EXECUTION LOOP ==========
 for TASK in "${TASKS[@]}"; do
